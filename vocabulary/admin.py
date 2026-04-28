26bd4f69806485dfa6ee93de4b1b66cac6b6b222
11x0f <amithprakash06@gmail.com>
2026-04-28 08:16:16 +0530

    Register Vocab app models in admin site

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import VocabularyWord, Phrase, GenzLingo, DailyVocabularyContent
+
+@admin.register(VocabularyWord)
+class VocabularyWordModel(admin.ModelAdmin):
+    list_display = [field.name for field in VocabularyWord._meta.fields]
+
+    list_filter = ('difficulty_level', 'category')
+    search_filter = ('word')
+
+@admin.register(Phrase)
+class PhraseModel(admin.ModelAdmin):
+    list_display = [field.name for field in Phrase._meta.fields]
+
+    list_filter = ('category', 'difficulty_level')
+    search_filter = ('phrase')
+
+@admin.register(GenzLingo)
+class GenzLingoModel(admin.ModelAdmin):
+    list_display = [field.name for field in GenzLingo._meta.fields]
+
+    list_filter = ('popularity_level', 'tone')
+    search_filter = ('term')
+
+@admin.register(DailyVocabularyContent)
+class DailyVocabularyContentModel(admin.ModelAdmin):
+    list_display = [ field.name for field in DailyVocabularyContent._meta.fields]
